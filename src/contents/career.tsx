a6af3542fb72914c674aa9346426d8a5d02f3d4d
Yuu0413s <y.shibata0820@gmail.com>
2025-07-18 14:04:16 +0900

    🎨 contents/career.tsxを作成

---
+++ src/contents/career.tsx
@@ -0,0 +1,17 @@
+import { VStack, Heading, Text } from "@Yamada-UI/react";
+
+const CareerSection = () => {
+    return(
+        <VStack as="section" id="career" alignItems="center" gap="lg">
+            <Heading as="h2" size="xl">
+                経歴
+            </Heading>
+            <VStack as="ul" listStyleType="none" gap="md" alignItems="flex-start">
+                <Text>2021/04~2024/03 都立翔陽高校</Text>
+                <Text>2024/04~ 武蔵野大学データサイエンス学部データサイエンス学科</Text>
+            </VStack>
+        </VStack>
+    )
+}
+
+export default CareerSection;
